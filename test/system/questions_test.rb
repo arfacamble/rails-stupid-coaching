596e8586480a8113e1045818d154a5ee62c92e48
Arthur Campbell <arfa.camble@gmail.com>
2021-03-27 09:44:37 +0000

    further test with screenshots taken

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -13,4 +13,12 @@ class QuestionsTest < ApplicationSystemTestCase
 
     assert_text "Silly question, get dressed and go to work!"
   end
+
+  test "expressing reluctance and fatigue brings ire from the coach" do
+    visit ask_url
+    fill_in "question", with: "Perhaps I shall snooze"
+    click_on "¡!ASK¡!"
+
+    take_screenshot
+  end
 end
